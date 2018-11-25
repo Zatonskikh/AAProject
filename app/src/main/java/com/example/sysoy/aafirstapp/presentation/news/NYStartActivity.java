22ff4ab460047976207fdd6477b9cecc525f7edb
CORP\Zatonskihev <zatonskihev@taxcom.ru>
2018-11-26 02:48:40 +0300

    implement init screen with view pager

--- app/src/main/java/com/example/sysoy/aafirstapp/presentation/news/NYStartActivity.java
+++ app/src/main/java/com/example/sysoy/aafirstapp/presentation/news/NYStartActivity.java
@@ -1,9 +1,17 @@
 package com.example.sysoy.aafirstapp.presentation.news;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.storage.StorageManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatImageView;
 
 import com.bumptech.glide.Glide;
@@ -14,28 +22,62 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Completable;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import me.relex.circleindicator.CircleIndicator;
 
-public class NYStartActivity extends AppCompatActivity{
+public class NYStartActivity extends AppCompatActivity {
 
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private static final int NUM_PAGES = 3;
+
+    ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState == null) {
-            setContentView(R.layout.start_activivty);
-            AppCompatImageView image = findViewById(R.id.loading_screen);
-            Glide
-                    .with(this)
-                    .load(R.drawable.ico_start)
-                    .into(image);
-            Disposable disposable = Completable.complete()
-                    .delay(3, TimeUnit.SECONDS)
-                    .subscribe(this::startSecondActivity);
-            compositeDisposable.add(disposable);
-        } else {
+
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+        if(!sharedPref.getBoolean("is_first_time", true)){
             startSecondActivity();
+        }else {
+            sharedPref.edit().putBoolean("is_first_time", false).apply();
         }
+        setContentView(R.layout.view_pager_start_screen);
+        mPager = findViewById(R.id.start_pager);
+        mPager.setBackgroundResource(android.R.color.holo_orange_dark);
+        mPagerAdapter = new StartPagerAdapter(getSupportFragmentManager()) {
+        };
+        mPager.setAdapter(mPagerAdapter);
+        CircleIndicator indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        mPager.setBackgroundResource(android.R.color.holo_orange_dark);
+                        break;
+                    case 1:
+                        mPager.setBackgroundResource(android.R.color.holo_blue_dark);
+                        break;
+                    case 2:
+                        mPager.setBackgroundResource(android.R.color.holo_purple);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+        AppCompatButton skipButton = findViewById(R.id.skip_button);
+        skipButton.setOnClickListener(view -> startSecondActivity());
+
     }
 
     private void startSecondActivity() {
@@ -49,5 +91,29 @@ public class NYStartActivity extends AppCompatActivity{
         compositeDisposable.dispose();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    class StartPagerAdapter extends FragmentStatePagerAdapter {
+        StartPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return StartFragment.newInstance(i);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/sysoy/aafirstapp/presentation/news/StartFragment.java
@@ -0,0 +1,64 @@
+package com.example.sysoy.aafirstapp.presentation.news;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sysoy.aafirstapp.R;
+
+public class StartFragment extends Fragment {
+
+    private static final String ARGS_POSITION = "args:position";
+
+    public static StartFragment newInstance(int position) {
+
+        Bundle args = new Bundle();
+        args.putInt(ARGS_POSITION, position);
+        StartFragment fragment = new StartFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private void screenChooser(int position, AppCompatImageView view, AppCompatTextView text){
+        switch (position){
+            case 0:
+                resourceSetter(view, text, R.drawable.read_news, R.string.read_news);
+                break;
+            case 1:
+                resourceSetter(view, text, R.drawable.ic_edit_black_24dp, R.string.update_news);
+                break;
+            case 2:
+                resourceSetter(view, text, R.drawable.ic_delete_white_24dp, R.string.delete_news);
+                break;
+        }
+    }
+
+    private void resourceSetter(AppCompatImageView view, AppCompatTextView text, int viewId, int textId){
+        view.setImageResource(viewId);
+        text.setText(textId);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(
+                R.layout.start_fragment, container, false);
+        int position =  getArguments().getInt(ARGS_POSITION, 0);
+        AppCompatImageView fragmentView = view.findViewById(R.id.fragment_image);
+        AppCompatTextView fragmentText = view.findViewById(R.id.fragment_text);
+        screenChooser(position, fragmentView, fragmentText);
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
